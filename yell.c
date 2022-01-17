135da0aa8eb57f60ed2f44d76e009294e95edfed
gkansala13 <kansalag@oregonstate.edu>
2022-01-17 00:35:12 -0800

    all 6 steps complete

--- yell.c
+++ yell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]){
 	char buffer[2048];
@@ -8,14 +9,21 @@ int main(int argc, char *argv[]){
 		int counter = 1;
 		while(counter > 0) {
 			counter = read(0, buffer, sizeof buffer);
+      for (int i = 0; i < counter; ++i) {
+				buffer[i] = toupper(buffer[i]);
+      }
 			write(1, buffer, counter);
 		}
 	}
+
 	for (int i = 1; i < argc; ++i) {
 		int file_descriptor = open(argv[i], O_RDONLY);	
 		int counter = 1;
 		while(counter > 0) {
 			counter = read(file_descriptor, buffer, sizeof buffer);
+      for (int i = 0; i < counter; ++i) {
+				buffer[i] = toupper(buffer[i]);
+      }
 			write(1, buffer, counter);
 		}
 	}
