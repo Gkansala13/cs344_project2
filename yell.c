1704b6e474905c67c3a2a0e3f1163ce7fc04660a
gkansala13 <kansalag@oregonstate.edu>
2022-01-17 00:41:22 -0800

    tried fixing indention

--- yell.c
+++ yell.c
@@ -4,27 +4,27 @@
 #include <ctype.h>
 
 int main(int argc, char *argv[]){
-	char buffer[2048];
+  char buffer[2048];
   if (argc < 2) {
-		int counter = 1;
-		while(counter > 0) {
-			counter = read(0, buffer, sizeof buffer);
+    int counter = 1;
+    while(counter > 0) {
+      counter = read(0, buffer, sizeof buffer);
       for (int i = 0; i < counter; ++i) {
-				buffer[i] = toupper(buffer[i]);
+        buffer[i] = toupper(buffer[i]);
       }
-			write(1, buffer, counter);
-		}
-	}
-
-	for (int i = 1; i < argc; ++i) {
-		int file_descriptor = open(argv[i], O_RDONLY);	
-		int counter = 1;
-		while(counter > 0) {
-			counter = read(file_descriptor, buffer, sizeof buffer);
+      write(1, buffer, counter);
+    }
+  }
+  
+  for (int i = 1; i < argc; ++i) {
+    int file_descriptor = open(argv[i], O_RDONLY);
+    int counter = 1;
+    while(counter > 0) {
+      counter = read(file_descriptor, buffer, sizeof buffer);
       for (int i = 0; i < counter; ++i) {
-				buffer[i] = toupper(buffer[i]);
+        buffer[i] = toupper(buffer[i]);
       }
-			write(1, buffer, counter);
-		}
-	}
+      write(1, buffer, counter);
+    }
+  }
 }
